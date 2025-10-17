89ba3789d54c950ab82f40b2c523c36ce0d708e9
Kishor052 <gowdakishor457@gmail.com>
2025-10-17 14:51:16 +0530

    api url changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
     formData.append('prompt', prompt);
 
     // Endpoint for your FastAPI backend
-    const API_URL = 'http://localhost:8000/upload-and-translate/';
+    const API_URL = 'https://docbot-o3u2.onrender.com/upload-and-translate/';
 
     try {
       const response = await fetch(API_URL, {
