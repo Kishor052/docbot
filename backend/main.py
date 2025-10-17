1a892def37c0423ff7916e5b18da3d3f82701612
Kishor052 <gowdakishor457@gmail.com>
2025-10-17 15:48:38 +0530

    ports changed

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 # Enable CORS for communication with the React front-end
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
